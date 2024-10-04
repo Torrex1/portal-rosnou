81312f330b47c349347286692d6e335faa1c4fda
Torrex1 <flendik666@gmail.com>
2024-10-04 20:55:18 +0300

    code refactoring for the router

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   import { ref } from 'vue'
   import Header from './components/Header.vue'
   import Menu from './components/Menu.vue'
-  import WelcomeCard from './components/WelcomeCard.vue'
-  import AdmissionsCard from './components/AdmissionsCard.vue'
-  import VaccinationPage from './components/VaccinationPage.vue'
 
   const sidebarOpen = ref(false);
   const openMenu = () => {
@@ -19,16 +16,9 @@
   <Menu v-if="sidebarOpen" @closeMenu="closeMenu" />
   <Header @openMenu="openMenu" />
 
-  <!-- <div style="padding: 1.5% 2.5%;  display: flex; flex-direction: column;">
-    <h1>Главная</h1>
-    <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 25px;">
-      <WelcomeCard />
-      <AdmissionsCard />
-      
-    </div>
-  </div> -->
-
-  <VaccinationPage />
+  <div style="padding: 1.5% 2.5%;  display: flex; flex-direction: column;">
+    <router-view />
+  </div>
   
 </template>
 
