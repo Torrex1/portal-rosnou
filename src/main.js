748448790aeb7e2ba31aa8d31fd92c42852f7185
Torrex1 <flendik666@gmail.com>
2024-10-04 20:52:28 +0300

    init vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+// import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue'
 
+import HomePage from './pages/HomePage.vue';
+import VaccinationPage from './pages/VaccinationPage.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage,
+        },
+        {
+            path: '/vaccination',
+            name: 'vaccination',
+            component: VaccinationPage,
+        },
+        
+    ]
+})
+
 const app = createApp(App);
-app.use(autoAnimatePlugin)
+app.use(router);
 app.mount('#app')
