4917062e74edd4b496db70d03f3dcd0c648b1b88
Torrex1 <flendik666@gmail.com>
2024-10-07 23:12:51 +0300

    create popup

---
+++ src/components/PopupComponents/Popup.vue
@@ -0,0 +1,52 @@
+<script setup>
+import SelectionButton from './SelectionButton.vue';
+
+const emit = defineEmits(['toggle_popup']);
+
+</script>
+
+<template>
+    <div @click="emit('toggle_popup')" class="popup">
+        <div @click.stop class="popup-wrapper">
+            <h2>Выберите один из пунктов (Впоследствии можно будет редактировать)</h2>
+
+            <SelectionButton />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.popup {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.382);
+    z-index: 10;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border: 1px solid red
+}
+
+.popup-wrapper {
+    position: fixed;
+    background-color: #fff;   
+    width: 20%;
+    height: auto;
+    z-index: 20;
+    padding: 10px 15px;
+    border-radius: 8px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+h2 {
+    text-align: center;
+}
+</style>
---
+++ src/components/PopupComponents/SelectionButton.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { reactive } from 'vue';
+
+let button_title_list = reactive([
+    'Переболел', "Имеются противопоказания", "Вакцинировался полностью", "Ревакцинировался", "Иностранный сертификат", 
+    "Вакцинировался 1-м компонентом", "Буду вакцинироваться", "Не буду вакцинироваться", "Нет 18-ти лет"
+]);
+</script>
+
+<template>
+    <button
+        v-for="title in button_title_list">{{ title }}
+    </button>
+</template>
+
+
+<style scoped> 
+button {
+        background-color: #f5f5f5;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 10px;
+        cursor: pointer;
+}
+
+button:hover {
+    background-color: #e6e6e6;
+}
+
+button:focus-visible {
+    background-color: #d9d9d9;
+    outline: none;
+    border: 1px solid red;
+    
+}
+</style>
